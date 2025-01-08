5d6b5463ac24c1d108f80f15c3a7b1ea514862ab
ErikaCetera <ceteraerika@gmail.com>
2025-01-08 17:02:39 +0100

    feat: rotta tags

--- app.js
+++ app.js
@@ -6,13 +6,23 @@ const app = express();
 const port = 3003 ;
 // Importa router dei posts
 const postsRouter = require("./routers/posts");
+// Importa router dei tags
+const tagsRouter = require("./routers/tags");
 // Importa funzione per gestire errore del server
-const handleError = require("./middlewares/handleError")
+const handleError = require("./middlewares/handleError");
+// importa cors
+const cors = require("cors");
+
+app.use(cors({
+    origin:"http://localhost:5173" 
+}));
 
 
 app.use(express.json())
 // Include tutte le rotte che inziano con /posts
 app.use("/posts", postsRouter);
+// Include tutte le rotte che inziano con /posts
+app.use("/tags", tagsRouter);
 
 // Rende accessibile il contenuto della cartella Public
 app.use(express.static('Public'));
